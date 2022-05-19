bd176182d006d4246d8230a7ff68260fa99e9c77
Xaferis Remote <mistrikmat@gmail.com>
2022-05-20 01:33:42 +0200

    implemented search - early stage, need refactoring

--- ToDoList/Screens/Search/SearchViewController.swift
+++ ToDoList/Screens/Search/SearchViewController.swift
@@ -8,11 +8,90 @@
 import UIKit
 
 class SearchViewController: UIViewController {
+    
+    @IBOutlet weak var tableView: UITableView!
+    let searchController = UISearchController(searchResultsController: nil)
+    
+    var filteredItems: [TaskModel] = []
+    
+    var items: [TaskModel] = []
+    
+    var isSearchBarEmpty: Bool {
+      return searchController.searchBar.text?.isEmpty ?? true
+    }
+    
+    var isFiltering: Bool {
+        return searchController.isActive && !isSearchBarEmpty
+    }
+
 
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        tableView.dataSource = self
+        tableView.register(
+            UINib(
+                nibName: TodoTaskTableViewCell.classString,
+                bundle: nil),
+            forCellReuseIdentifier: TodoTaskTableViewCell.classString)
+        
+        searchController.searchResultsUpdater = self
+        searchController.obscuresBackgroundDuringPresentation = false
+        searchController.searchBar.placeholder = "Search tasks"
+        navigationItem.searchController = searchController
+        definesPresentationContext = true
+        
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        TodoListManager.shared.loadTasks {
+            items = TodoListManager.shared.getTasks()[0].tasks
+        }
+    }
+    
+    func filterContentForSearchText(_ searchText: String) {
+        filteredItems = items.filter({ item in
+            return item.name.lowercased().contains(searchText.lowercased())
+        })
+        
+        tableView.reloadData()
+        
     }
 
+}
+
+//MARK: - Data source
+
+extension SearchViewController: UITableViewDataSource {
+    func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        if isFiltering {
+            return filteredItems.count
+        }
+        
+        return items.count
+    }
+    
+    
+    func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        guard let taskCell = tableView.dequeueReusableCell(withIdentifier: TodoTaskTableViewCell.classString,
+                                                           for: indexPath) as? TodoTaskTableViewCell
+        else {
+            return UITableViewCell()
+        }
+        
+        if isFiltering {
+            taskCell.setupCell(with: filteredItems[indexPath.row], at: (indexPath.section, indexPath.row))
+        } else {
+            taskCell.setupCell(with: items[indexPath.row], at: (indexPath.section, indexPath.row))
+        }
+        //taskCell.buttonDelegate = self
+        return taskCell
+    }
+}
+
+extension SearchViewController: UISearchResultsUpdating {
+    func updateSearchResults(for searchController: UISearchController) {
+        let searchBar = searchController.searchBar
+        filterContentForSearchText(searchBar.text!)
+    }
 }
